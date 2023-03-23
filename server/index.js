f03adfb6500c1b974206b28d5d18ce849524d3eb
ilyansoul <ilyan.kortbi.ifcha@gmail.com>
2023-03-23 13:28:17 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+const port = process.env.PORT
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(port, () => console.log('Server started on port ' + port));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
