c6f46400a75afacce3a3f960676eb5419d513a48
ilyansoul <ilyan.kortbi.ifcha@gmail.com>
2023-03-24 13:03:51 +0200

    first commit

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -27,7 +27,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:5001/api/v1/dalle', {
+        const response = await fetch('https://mid.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -55,7 +55,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:5001/api/v1/post', {
+        const response = await fetch('https://mid.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5001/api/v1/post', {
+      const response = await fetch('https://mid.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
